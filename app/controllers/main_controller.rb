861633a4bd2515b534698fccbbba7996a6da3cb7
nafi <nafidurmus07@gmail.com>
2020-03-19 23:43:14 +0300

    finish world part

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,14 @@
 class MainController < ApplicationController
+
 	def index
+		require 'open-uri'
+		require 'nokogiri'
+		doc = Nokogiri::HTML(open("https://www.worldometers.info/coronavirus/"))
+		@world = doc.css('div.maincounter-number')		
 	end
 
 	def fetch_data
+		
+
 	end
 end
